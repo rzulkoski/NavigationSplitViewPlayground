2835f43a6eab5716823177686baab246b118cd6d
Ryan Zulkoski <ryan.zulkoski@buildertrend.com>
2023-03-22 13:30:17 -0500

    Initial Commit

---
+++ NavigationSplitVIewPlayground/NavigationSplitVIewPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitVIewPlaygroundApp.swift
+//  NavigationSplitVIewPlayground
+//
+//  Created by Ryan Zulkoski on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitVIewPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
